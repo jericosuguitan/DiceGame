06b3884f7311debe79dc7278ef0795c1fef1ada0
Jerico Suguitan <jerico.suguitan@gmail.com>
2019-06-07 18:28:30 -0700

    random not really generator

--- DiceGame.cpp
+++ DiceGame.cpp
@@ -1,9 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+
+
 int roll(){
-    
+       srand((int)time(0));
+    int value = (int) ((rand() % 6) + 1);
+    return value;
 }
 
-int main (){
-
+int main () {
+   for (int i = 0; i < 1; i++){
+       int result = roll();
+   cout << "Dice roll result = " << result << endl;
+   }
+   return 0;
 }
+
+/*
+int main()
+{
+srand(time(0));
+for (int i = 0; i<10; i++)
+{
+int dice = (int) (1+rand()%6);
+cout << dice << endl;
+}
+return 0;
+}
+*/
